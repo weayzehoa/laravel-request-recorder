8be72f00d6be9a659d5a9e7689aece299d6c6598
gene <gene@js-tech.tw>
2018-01-15 11:16:41 +0800

    新增排除路由記錄方法，排除對應路由 http code config

--- src/Middleware/RequestRecorderMiddleware.php
+++ src/Middleware/RequestRecorderMiddleware.php
@@ -4,6 +4,7 @@ namespace LittleBookBoy\Request\Recorder\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use LittleBookBoy\Request\Recorder\Models\RequestRecords;
 use Ramsey\Uuid\Uuid;
 
@@ -43,7 +44,7 @@ class RequestRecorderMiddleware
             return Response()->make('', 409);
         }
 
-        // 檢查是否要記錄此請求
+        // 檢查是否要記錄此請求：方法 isExceptMethod 為真表示不記錄
         if ($this->isExceptMethod($request->getMethod())) {
             return $next($request);
         }
@@ -51,6 +52,11 @@ class RequestRecorderMiddleware
         // 加上回應表頭
         $response = $next($request);
         $response->headers->add([self::X_CORRELATION_ID => $this->requestId]);
+
+        // 檢查是否要記錄此請求：路由、http code isExceptRoutes 為真表示不記錄
+        if ($this->isExceptRoutes($request->route()->getName(), $response->status())) {
+            return $next($request);
+        }
 
         // 在收到請求時就建立記錄
         $this->storeApiResponseLog($request, $response);
@@ -115,10 +121,44 @@ class RequestRecorderMiddleware
     }
 
     /**
-     * 檢查是否不要記錄此請求
+     * 檢查是否不要記錄此請求： http 方法名 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'
+     * @return bool false 表示記錄該請求方法，true 表示不記錄（排除掉該方法的所有請求）
      */
     protected function isExceptMethod($method)
     {
+        // 使用者在 config 中沒有設定 except 則回應 false，記錄該請求資訊
+        if (empty(config('request-recorder.recorder.except'))) {
+            return false;
+        }
+
         return in_array(strtoupper($method), config('request-recorder.recorder.except'));
+    }
+
+    /**
+     * 檢查是否不要記錄此請求：路由名、http code 如 200、409
+     * @return bool false 表示記錄該請求方法，true 表示不記錄（排除掉該方法的所有請求）
+     */
+    protected function isExceptRoutes($routeName, $responseCode)
+    {
+        $skipRoutes = collect(config('request-recorder.recorder.skip_routes'))->groupBy('route_name');
+        if ($skipRoutes->isEmpty()) {
+            return false;
+        }
+
+        // 使用者在 config 中有匹配到目前請求路由，進入檢查 http code 的設定
+        if (array_key_exists($routeName, $skipRoutes->toArray())) {
+            $dataSet = $skipRoutes->get($routeName)->first();
+
+            // 當 config 中對應該路由的 http code 的設定為空或萬用，所有對應該請求的路由都記錄起來
+            if (empty(array_get($dataSet, 'http_code')) || in_array('*', array_get($dataSet, 'http_code'))) {
+                return true;
+            }
+
+            if (in_array($responseCode, array_get($dataSet, 'http_code'))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- src/resources/config/request-recorder.php
+++ src/resources/config/request-recorder.php
@@ -7,10 +7,21 @@ return [
      * - enabled : true or false
      * - group : route middleware group name
      * - except : 僅記錄除了這些方法之外的請求, 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'
+     * - skip_routes : 僅記錄除了這些路由之外的請求, 也可限定只排除該路由的某些 rsponse http code
      */
     'recorder' => [
         'enabled' => true,
         'group' => 'api',
-        'except' => ['']
+        'except' => [''],
+        'skip_routes' => [
+            [
+                'route_name' => 'route.name1',
+                'http_code' => ['*']
+            ],
+            [
+                'route_name' => 'route.name2',
+                'http_code' => ['409', '422']
+            ],
+        ]
     ]
 ];
