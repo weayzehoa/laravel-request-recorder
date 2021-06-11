16f23b1fb9cf4f83b577808d705c689e4c2ed937
Roger Wu <43947465+weayzehoa@users.noreply.github.com>
2021-06-11 16:07:51 +0800

    Update RequestRecorderMiddleware.php
    modify RequestRecorderMiddleware.php to fit for Laravel 8.x

--- src/Middleware/RequestRecorderMiddleware.php
+++ src/Middleware/RequestRecorderMiddleware.php
@@ -150,14 +150,27 @@ class RequestRecorderMiddleware
             $dataSet = $skipRoutes->get($routeName)->first();
 
             // 當 config 中對應該路由的 http code 的設定為空或萬用，所有對應該請求的路由都記錄起來
-            if (empty(array_get($dataSet, 'http_code')) || in_array('*', array_get($dataSet, 'http_code'))) {
+            if (empty($dataSet['http_code']) || in_array('*', $dataSet['http_code'])) {
                 return true;
             }
-
-            if (in_array($responseCode, array_get($dataSet, 'http_code'))) {
+            if (in_array($responseCode, $dataSet['http_code'])) {
                 return true;
             }
         }
+
+        // // 使用者在 config 中有匹配到目前請求路由，進入檢查 http code 的設定
+        // if (array_key_exists($routeName, $skipRoutes->toArray())) {
+        //     $dataSet = $skipRoutes->get($routeName)->first();
+
+        //     // 當 config 中對應該路由的 http code 的設定為空或萬用，所有對應該請求的路由都記錄起來
+        //     if (empty(array_get($dataSet, 'http_code')) || in_array('*', array_get($dataSet, 'http_code'))) {
+        //         return true;
+        //     }
+
+        //     if (in_array($responseCode, array_get($dataSet, 'http_code'))) {
+        //         return true;
+        //     }
+        // }
 
         return false;
     }
